ce3368a0109a203a41e8d90f0d93176eca902a2f
Ade Yusuf <adeyusuf005@gamil.com>
2024-04-15 11:32:49 +0700

    Update responsive navbar in website

--- components/share/consultation.tsx
+++ components/share/consultation.tsx
@@ -6,7 +6,7 @@ export default function ConsultationComponent() {
     <div className="w-full h-full bg-white mt-10">
       <Button
         variant="outline"
-        className="uppercase lg:text-4xl text-md border-4 border-green-500 lg:py-5 py-2 px-2 rounded-full font-bold h-full text-start flex lg:gap-3 gap-0 items-center"
+        className="uppercase lg:text-4xl text-md border-4 border-green-500 lg:py-5 py-2 lg:px-5 px-2 rounded-full font-bold h-full text-start flex lg:gap-3 gap-0 items-center"
       >
         <div className="lg:w-32 w-20 lg:h-32 h-20 flex justify-center rounded-full items-center bg-green-500">
           <FaWhatsapp className="text-white lg:text-7xl text-5xl" />
