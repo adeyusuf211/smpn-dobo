4bf401eaaa2d2cab54c2e05692bb43f0216795b5
Ade Yusuf <adeyusuf005@gamil.com>
2024-05-01 14:42:54 +0700

    (01/05/2024) ADD: menambahkan halaman redaksi pendidik dan pengumuma

--- app/(landing-page)/(librari)/pengumuman/page.tsx
+++ app/(landing-page)/(librari)/pengumuman/page.tsx
@@ -1,5 +1,89 @@
-import UnderConstructionComponent from "@/components/share/under-construction";
+import CardWithImageComponent from "@/components/share/card-with-image";
+import FooterComponent from "@/components/share/footer";
+
+import ExampleImage6 from "@/public/assets/images/example-6.svg";
 
 export default function PengumumanPage() {
-  return <UnderConstructionComponent />;
+  return (
+    <div className="w-full h-full min-h-screen">
+      <h1 className="2xl:text-6xl lg:text-4xl text-2xl uppercase text-center font-semibold text-white mt-48">
+        Pengumuman
+      </h1>
+      <div className="my-20 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 justify-center">
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="1 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="2 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="3 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="4 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="1 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="2 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="3 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="4 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="1 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="2 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="3 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          badge="4 Mei 2024"
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+      </div>
+      <FooterComponent />
+    </div>
+  );
 }
--- components/share/card-with-image.tsx
+++ components/share/card-with-image.tsx
@@ -1,26 +1,44 @@
 import Image from "next/image";
 
 interface CardWithImageComponentProps {
+  readonly badge?: string;
   readonly imageSource: string;
   readonly title?: string;
+  readonly description?: string;
 }
 
 export default function CardWithImageComponent({
+  badge,
   imageSource,
   title,
+  description,
 }: CardWithImageComponentProps) {
   return (
     <div className="flex flex-col gap-10 bg-white p-5">
       <div className="flex justify-center items-center w-full min-h-[200px] h-full">
         <Image src={imageSource} alt="model" loading="lazy" />
       </div>
-      {title ? (
-        <div className="flex justify-start">
-          <h1 className="font-semibold text-2xl text-gray-700 uppercase">
-            {title}
-          </h1>
-        </div>
-      ) : null}
+      <div className="flex flex-col gap-2">
+        {badge ? (
+          <small className="w-full">
+            <span className="font-semibold">Terbit : </span> {badge}
+          </small>
+        ) : null}
+        {title ? (
+          <div className="flex justify-start">
+            <h1 className="font-semibold text-2xl text-gray-700 uppercase">
+              {title}
+            </h1>
+          </div>
+        ) : null}
+        {description ? (
+          <div className="flex justify-start">
+            <h1 className="font-semibold text-base text-gray-700">
+              {description}
+            </h1>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
