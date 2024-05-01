9d18c1d5cf9a317a2b1702f507454fcc5179bf93
Ade Yusuf <adeyusuf005@gamil.com>
2024-05-01 14:50:06 +0700

    (01/05/2024) ADD: Menambahkan halaman pengembangan diri

--- app/(landing-page)/(warga-sekolah)/pengembangan-diri/page.tsx
+++ app/(landing-page)/(warga-sekolah)/pengembangan-diri/page.tsx
@@ -1,5 +1,57 @@
-import UnderConstructionComponent from "@/components/share/under-construction";
+import CardWithImageComponent from "@/components/share/card-with-image";
+import FooterComponent from "@/components/share/footer";
+
+import ExampleImage6 from "@/public/assets/images/example-6.svg";
 
 export default function PengembanganDiriPage() {
-  return <UnderConstructionComponent />;
+  return (
+    <div className="w-full h-full min-h-screen">
+      <h1 className="2xl:text-6xl lg:text-4xl text-2xl uppercase text-center font-semibold text-white mt-48">
+        Pengembangan Diri (Eskul)
+      </h1>
+      <div className="my-20 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 justify-center">
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage6}
+          title="Your Info Title Here"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolore?"
+        />
+      </div>
+      <FooterComponent />
+    </div>
+  );
 }
