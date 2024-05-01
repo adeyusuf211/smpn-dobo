cda399e3c41671ad27a5f2f01a35a78fe87ecf31
Ade Yusuf <adeyusuf005@gamil.com>
2024-05-01 07:27:58 +0700

    (01/05/2024) ADD: menambahkan halamangaleri foto

--- app/(landing-page)/(librari)/galeri-foto/page.tsx
+++ app/(landing-page)/(librari)/galeri-foto/page.tsx
@@ -1,5 +1,81 @@
-import UnderConstructionComponent from "@/components/share/under-construction";
+import Image from "next/image";
+
+import CardWithImageComponent from "@/components/share/card-with-image";
+
+import ExampleImage1 from "@/public/assets/images/example-1.svg";
+import ExampleImage2 from "@/public/assets/images/example-2.svg";
+import ExampleImage3 from "@/public/assets/images/example-3.svg";
+import ExampleImage4 from "@/public/assets/images/example-4.svg";
+import ExampleImage5 from "@/public/assets/images/example-5.svg";
 
 export default function GaleriFotoPage() {
-  return <UnderConstructionComponent />;
+  return (
+    <div className="w-full h-full min-h-screen">
+      <h1 className="2xl:text-6xl lg:text-4xl text-2xl uppercase text-center font-semibold text-white mt-48">
+        Arsip Foto
+      </h1>
+      <div className="my-20 grid 2xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 justify-center">
+        <CardWithImageComponent
+          imageSource={ExampleImage1}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage2}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage3}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage4}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage5}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage1}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage2}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage3}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage4}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage5}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage1}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage2}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage3}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage4}
+          title="Your title for image here"
+        />
+        <CardWithImageComponent
+          imageSource={ExampleImage5}
+          title="Your title for image here"
+        />
+      </div>
+    </div>
+  );
 }
---
+++ components/share/card-with-image.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+interface CardWithImageComponentProps {
+  readonly imageSource: string;
+  readonly title?: string;
+}
+
+export default function CardWithImageComponent({
+  imageSource,
+  title,
+}: CardWithImageComponentProps) {
+  return (
+    <div className="flex flex-col gap-10 bg-white p-5">
+      <div className="flex justify-center items-center w-full min-h-[200px] h-full">
+        <Image src={imageSource} alt="model" />
+      </div>
+      {title ? (
+        <h1 className="font-semibold text-2xl text-gray-700 uppercase">
+          {title}
+        </h1>
+      ) : null}
+    </div>
+  );
+}
