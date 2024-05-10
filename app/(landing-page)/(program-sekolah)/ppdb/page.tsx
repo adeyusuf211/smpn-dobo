b9d72ac69d904b1227d923784052655ca9b406c7
Ade Yusuf <adeyusuf005@gamil.com>
2024-05-10 08:03:48 +0700

    (10/05/2024) UPDATE: menambahkan metadata pada halaman PPDB

--- app/(landing-page)/(program-sekolah)/ppdb/page.tsx
+++ app/(landing-page)/(program-sekolah)/ppdb/page.tsx
@@ -1,5 +1,12 @@
 import PPDBComponent from "@/components/share/pages/ppdb";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SMP NEGERI 1 DOBO | PPDB",
+  description: "Halaman Penerimaan Peserta Didik Baru (PPDB) SMP Negeri 1 DOBO",
+};
+
 export default function PPDBPage() {
   return <PPDBComponent />;
 }
