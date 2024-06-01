a63e7df5ca90644702b905fb0fec1c3ec2739832
Ade Yusuf <adeyusuf005@gamil.com>
2024-06-01 20:40:38 +0700

    Update

--- components/share/hero.tsx
+++ components/share/hero.tsx
@@ -7,7 +7,7 @@ interface HeroProps {
 import Image from "next/image";
 import TaglineComponent from "./tagline";
 import ModelSekolahImage from "@/public/assets/images/Foto Siswa/model.webp";
-import LogoBANSM from "@/public/assets/images/logo ban-sm.png";
+import ImageLogo from "@/public/assets/images/logo1.png";
 
 import { useAOS } from "@/hooks/useAOS";
 
@@ -18,8 +18,8 @@ export default function HeroComponent({ showAddressLabel }: HeroProps) {
     <div className="w-full h-screen relative" data-aos="fade-up">
       <div className="flex md:flex-row flex-col justify-between">
         <div className="w-full h-full flex justify-center items-center">
-          <div className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] flex justify-center items-center absolute lg:top-0 top-10 right-0 lg:mt-[300px] mt-[80px] mr-[10%] rounded-full">
-            <Image src={LogoBANSM} width={300} height={300} alt="logo ban sm" />
+          <div className="lg:w-[200px] flex justify-center items-center absolute lg:top-0 top-10 right-0 lg:mt-[300px] mt-[80px] mr-[10%] rounded-full">
+            <Image src={ImageLogo} width={1000} height={1000} alt="logo" />
           </div>
         </div>
         <div
