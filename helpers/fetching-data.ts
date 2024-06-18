c6537ec174c65d9952901c44e3e7c774e5331909
Ade Yusuf <adeyusuf005@gamil.com>
2024-06-18 14:16:10 +0700

    Update

--- helpers/fetching-data.ts
+++ helpers/fetching-data.ts
@@ -3,7 +3,12 @@ import axios from "axios";
 export async function getDataBuku() {
   try {
     const response = await axios.get(
-      `http://admin.smpnegeri1dobo.sch.id/api/books/list`
+      `http://admin.smpnegeri1dobo.sch.id/api/books/list`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
     );
     return response;
   } catch (error) {
